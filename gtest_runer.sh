e634c30f98897df51a32ac75b3c7313183e874ec
Anton <tokha.mamiedov@gmail.com>
2020-10-30 13:57:13 +0300

    SUPPER LAST

--- gtest_runer.sh
+++
@@ -1,2 +0,0 @@
-valgrind --tool=memcheck --leak-check=yes ./filemetrics/build/unit_tests/Google_Tests_run
-valgrind --tool=memcheck --leak-check=yes ./filemetrics_multithreaded/build/unit_tests/Google_Tests_run
